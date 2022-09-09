9d9a489a1bc559bc771789be5e69580237e39581
Samurai-way <olegmoishevych@gmail.com>
2022-09-09 19:00:40 +0300

    counter_version_3

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import app from './App.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const App = () => {
 
-export default App;
+    const [value, setValue] = useState(0)
+    const [disabled, setDisabled] = useState(false)
+    const [start, setStart]=useState(0)
+
+    const onclickInc = () => {
+        setValue(value + 1)
+        if (value === 5) return setDisabled(!disabled)
+    }
+    const onclickDec = () => {
+        setValue(0)
+    }
+
+    const onclickSave = () => {
+        localStorage.setItem('Save:',JSON.stringify(start))
+        let str = localStorage.getItem('SaveValue')
+        if(str){
+            let newValue = JSON.parse(str)
+            setValue(newValue)
+        }
+    }
+
+
+    return (
+        <div className={app.wrapper}>
+            {value}
+            <button disabled={disabled} onClick={onclickInc}>inc</button>
+            <button onClick={onclickDec}>dec</button>
+            <button onClick={onclickSave}>save</button>
+        </div>
+    );
+};
